1fc295bdcd31fbb7f5516401f89b5a471f5107d9
Aditya Galang Adzana <aditya.galang.adzana@polytron.co.id>
2023-07-10 14:15:35 +0700

    view space

--- Ultimate Shadergraph Guide/Assets/Section 5/DebugVertices.cs
+++ Ultimate Shadergraph Guide/Assets/Section 5/DebugVertices.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [ExecuteInEditMode]
 public class DebugVertices : MonoBehaviour {
@@ -17,9 +18,11 @@ public class DebugVertices : MonoBehaviour {
 		}
 		foreach (Vector3 v in vertices)
 		{
+			Vector3 viewVertValue = SceneView.GetAllSceneCameras()[0].WorldToViewportPoint(transform.position + v);
 			string vert = "v: " + v.ToString();
 			string worldVert = "wv : " + (transform.position + v);
-			UnityEditor.Handles.Label(transform.position + v, vert + worldVert);
+			string viewVert = "vv : " + viewVertValue;
+			UnityEditor.Handles.Label(transform.position + v, vert + worldVert + viewVert);
 
 		}
 	}
